4ba685f091378cb3b9edbaab9cff8e1ddf9dbe6f
Anver Housseini <anver@anykey.ch>
2021-04-18 09:19:01 +0200

    update

--- Casks/easytax-ag.rb
+++ Casks/easytax-ag.rb
@@ -1,19 +1,19 @@
-cask 'easytax-ag' do
-  version '2019,1.3'
-  sha256 '29b435d67dc04140cd427ae10f33a3267d6f71bda23f0c4b16730dbc9875af26'
+cask "easytax-ag" do
+  version "2020,1.3"
+  sha256 "2cfad8e54d009ad4f5eff85b8d63b18b1ff78271a0ff7ee74c466544d32eb59e"
 
   url "https://msg-easytax.ch/ag/#{version.before_comma}/EasyTax#{version.before_comma}AG_macos_#{version.after_comma.dots_to_underscores}_mitJRE.dmg"
-  name 'EasyTax AG'
-  homepage 'https://msg-easytax.ch/'
+  name "EasyTax AG"
+  homepage "https://msg-easytax.ch/"
 
   installer script: {
-                      executable: "EasyTax #{version.before_comma} AG Installationsprogramm.app/Contents/MacOS/JavaApplicationStub",
-                      args:       ['-q'],
-                    }
+    executable: "EasyTax #{version.before_comma} AG Installationsprogramm.app/Contents/MacOS/JavaApplicationStub",
+    args:       ["-q"],
+  }
 
   uninstall script: {
-                      executable: "/Applications/EasyTax/AG#{version.before_comma}/EasyTax #{version.before_comma} AG Deinstallationsprogramm.app/Contents/MacOS/JavaApplicationStub",
-                      args:       ['-q'],
-                    },
-            rmdir:  '/Applications/EasyTax'
+    executable: "/Applications/EasyTax/AG#{version.before_comma}/EasyTax #{version.before_comma} AG Deinstallationsprogramm.app/Contents/MacOS/JavaApplicationStub",
+    args:       ["-q"],
+  },
+            rmdir:  "/Applications/EasyTax"
 end
