472d758b881c94e162288a9c78a041585134b4cf
Anver Housseini <contact@anver.ch>
2024-06-02 21:07:43 +0200

    Update easytax-ag.rb

--- Casks/easytax-ag.rb
+++ Casks/easytax-ag.rb
@@ -1,6 +1,6 @@
 cask "easytax-ag" do
-  version "2021,1.2"
-  sha256 "86bdf36f9e3b69669e25273b921b770f324f1ac11454b08671233b9cd08e3dc6"
+  version "2023,1.2"
+  sha256 "198d83d4c27be38c9ddc480c2d8eb516655c3cde45217d3381e2ee36c01e8ea9"
 
   url "https://msg-easytax.ch/ag/#{version.before_comma}/EasyTax#{version.before_comma}AG_macos_#{version.after_comma.dots_to_underscores}_mitJRE.dmg"
   name "EasyTax AG"
